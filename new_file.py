b6c75e5874a7a28af4ae54f842a68f55ced04eca
Pham Thuy <thuy.pham@edu.turkuamk.fi>
2025-04-12 17:21:27 +0300

    hi

--- new_file.py
+++ new_file.py
@@ -3,8 +3,4 @@ print("This is a new file!!!!")
 
 print("We made some changes to this file!!!!!!!!!!!")
 
-print("This is a new line added to the file")
 
-
-
-print("This is a new line added to the file")
